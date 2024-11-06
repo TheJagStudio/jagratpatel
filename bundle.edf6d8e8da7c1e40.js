d3861d8e2e03aa6634e398310c53eadbca0cf5ce
Jagrat Patel <81472856+TheJagStudio@users.noreply.github.com>
2024-11-06 22:38:42 +0530

    Update bundle.edf6d8e8da7c1e40.js

--- bundle.edf6d8e8da7c1e40.js
+++ bundle.edf6d8e8da7c1e40.js
@@ -20607,7 +20607,7 @@
                 this.experience = new Jf, this.gestures = this.experience.gestures, this.render = this.experience.ui.work.render, this.sounds = this.experience.sounds, this.scroll = this.experience.ui.scroll, this.sizes = this.experience.sizes, this.addButtonEventListeners(), this.initSwipes(), this.updatePositions(!0), this.onArrowClick(), this.sizes.on("portrait", (() => this.onOrientationChange())), this.sizes.on("landscape", (() => this.onOrientationChange()))
             }
             onOrientationChange() {
-                this.currentItemIndex = 2, this.updatePositions()
+                this.currentItemIndex = 5, this.updatePositions()
             }
             addButtonEventListeners() {
                 this.domElements.backButton.addEventListener("click", (() => {
